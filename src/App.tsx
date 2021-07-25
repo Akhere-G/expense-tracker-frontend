bb182e2cfefd7c0e27dd82f537a7230cd6463738
Akhere Ihoeghinlan <101akhere5@gmail.com>
2021-07-25 12:29:35 +0100

    basic page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import {AddTransactionForm,Balance,Footer,Header,Transactions} from "./components"
 function App () {
-  return <div>app</div>
+  return <div>
+    <Header/>
+    <Balance/>
+    <AddTransactionForm />
+    <Transactions/>
+    <Footer />
+  </div>
 }
 
 export default App
