8614ee84dd90683419986a0ae34301112b50e4cf
Akhere Ihoeghinlan <101akhere5@gmail.com>
2021-08-29 15:15:03 +0100

    refactored app state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { actionCreators } from "./actions/transactionActions";
-import { TransactionState } from "./reducers/transactionReducer";
 import {
   AddTransactionForm,
   Balance,
@@ -12,64 +9,12 @@ import {
   Modal,
 } from "./components";
 import { Main } from "./global";
-import {
-  Transaction as ITransaction,
-  Category,
-  Balance as IBalance,
-} from "./types";
 
-const initialTransactions: ITransaction[] = [
-  {
-    id: "1",
-    amount: "10",
-    type: "expense",
-    description: "Shopping",
-    category: Category.Groceries,
-    date: "2021-01-02",
-  },
-  {
-    id: "2",
-    amount: "35",
-    type: "income",
-    description: "gift",
-    category: Category.Misc,
-    date: "2021-03-07",
-  },
-  {
-    id: "3",
-    amount: "42",
-    type: "expense",
-    description: "Water bill",
-    category: Category.Utilities,
-    date: "2021-06-23",
-  },
-  {
-    id: "4",
-    amount: "50",
-    type: "income",
-    description: "gift from work",
-    category: Category.Invoice,
-    date: "2021-01-02",
-  },
-];
+import { useExpenseTracker } from "./hooks";
 
 const App = () => {
-  const dispatch = useDispatch();
-  const { addTransaction, setTransactions } = actionCreators;
-  useEffect(() => {
-    dispatch(setTransactions(initialTransactions));
-  }, [dispatch, setTransactions]);
-
-  const transactions = useSelector<TransactionState>(
-    (state) => state.transactions
-  ) as ITransaction[];
-
-  const balance = useSelector<TransactionState>(
-    (state) => state.balance
-  ) as IBalance;
-
-  const [show, setShow] = useState(true);
-
+  const { balance, show, setShow, addTransaction, transactions } =
+    useExpenseTracker();
   return (
     <>
       <Header />
@@ -78,11 +23,7 @@ const App = () => {
         <Modal show={show} setShow={setShow}>
           <h2>Delete</h2>
         </Modal>
-        <AddTransactionForm
-          addTransaction={(transaction: ITransaction) =>
-            dispatch(addTransaction(transaction))
-          }
-        />
+        <AddTransactionForm addTransaction={addTransaction} />
         <Transactions transactions={transactions} />
       </Main>
       <Footer />
