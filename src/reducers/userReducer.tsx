470cb35c55a59139809fb3aa5a062e4c89524eb0
Akhere Ihoeghinlan <101akhere5@gmail.com>
2021-09-17 00:09:28 +0100

    adds token to user state

--- src/reducers/userReducer.tsx
+++ src/reducers/userReducer.tsx
@@ -11,11 +11,13 @@ import { User } from "../utils/types";
 export type UserState = {
   user: User | null;
   message: string | null;
+  token: string | null;
 };
 
 const initialState: UserState = {
   user: null,
   message: null,
+  token: null,
 };
 
 export const userReducer = (
@@ -25,12 +27,12 @@ export const userReducer = (
   switch (action.type) {
     case LOGIN_SUCCESS:
     case REGISTER_SUCCESS:
-      return { user: action.payload.user, message: null };
+      return { user: action.payload.user, token: action.payload.token, message: null };
     case LOGIN_FAILURE:
     case REGISTER_FAILURE:
       return { ...state, message: action.payload.message };
     case LOGOUT:
-      return { ...state, user: null };
+      return { ...state, user: null, token: null };
     default:
       return state;
   }
