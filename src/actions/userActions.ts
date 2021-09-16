5b336bbd9d6f3a8722518e28584c00ab29928e24
Akhere Ihoeghinlan <101akhere5@gmail.com>
2021-09-16 23:32:28 +0100

    adds register actions

--- src/actions/userActions.ts
+++ src/actions/userActions.ts
@@ -4,6 +4,9 @@ import * as api from "../utils/api";
 
 export const LOGIN_SUCCESS = "user/LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "user/LOGIN_FAILURE";
+export const REGISTER_SUCCESS = "user/REGISTER_SUCCESS";
+export const REGISTER_FAILURE = "user/REGISTER_FAILURE";
+
 export const LOGOUT = "user/LOGOUT";
 
 type Actions = {
@@ -13,6 +16,14 @@ type Actions = {
   };
   [LOGIN_FAILURE]: {
     type: typeof LOGIN_FAILURE;
+    payload: { message: string };
+  };
+  [REGISTER_SUCCESS]: {
+    type: typeof REGISTER_SUCCESS;
+    payload: { user: User; token: string };
+  };
+  [REGISTER_FAILURE]: {
+    type: typeof REGISTER_FAILURE;
     payload: { message: string };
   };
   [LOGOUT]: {
@@ -35,6 +46,17 @@ export const actionCreators = {
     type: LOGIN_FAILURE,
     payload: { message },
   }),
+  registerSuccess: (
+    user: User,
+    token: string
+  ): Actions[typeof REGISTER_SUCCESS] => ({
+    type: REGISTER_SUCCESS,
+    payload: { user, token },
+  }),
+  registerFailure: (message: string): Actions[typeof REGISTER_FAILURE] => ({
+    type: REGISTER_FAILURE,
+    payload: { message },
+  }),
   logout: (): Actions[typeof LOGOUT] => ({ type: LOGOUT }),
   login: (formData: LoginData) => async (dispatch: Dispatch<RootAction>) => {
     try {
@@ -45,6 +67,17 @@ export const actionCreators = {
     } catch (err: any) {
       const message = err?.response?.data?.message || "Could not login.";
       dispatch(actionCreators.loginFailure(message));
+    }
+  },
+  register: (formData: LoginData) => async (dispatch: Dispatch<RootAction>) => {
+    try {
+      const response = await api.register(formData);
+      const { user, token } = response.data as LoginResponse;
+
+      dispatch(actionCreators.registerSuccess(user, token));
+    } catch (err: any) {
+      const message = err?.response?.data?.message || "Could not register.";
+      dispatch(actionCreators.registerFailure(message));
     }
   },
 };
