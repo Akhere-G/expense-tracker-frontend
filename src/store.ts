88bf3977968ee700c72193060f97ac02772af419
Akhere Ihoeghinlan <101akhere5@gmail.com>
2021-09-21 15:36:39 +0100

    only adds redux dev tools if available and in development

--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,28 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose, Store } from "redux";
 import thunkMiddleware from "redux-thunk";
 
 import { rootReducer } from "./reducers/rootReducer";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunkMiddleware),
-    (window as any)?.__REDUX_DEVTOOLS_EXTENSION__ &&
-      (window as any)?.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+import { isInDevelopment } from "./utils/utils";
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__?: Function;
+  }
+}
+
+let store: Store;
+
+if (isInDevelopment() && window.__REDUX_DEVTOOLS_EXTENSION__) {
+  store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(thunkMiddleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+} else {
+  store = createStore(rootReducer, compose(applyMiddleware(thunkMiddleware)));
+}
 
 export default store;
