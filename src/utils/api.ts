116ffe866bdb21df4f737ee4baac37a251b3cecf
Akhere Ihoeghinlan <101akhere5@gmail.com>
2021-09-21 02:50:40 +0100

    fixes api call

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -11,11 +11,9 @@ export interface FetchTransactions {
 export interface TransactionResponse {
   transaction: Transaction;
 }
-export const getDomain = () => {
-  const isDev = isInDevelopment();
-  console.log("printing", isDev, process.env.REACT_APP_PROD_API, process.env);
-  return isDev ? "http://localhost:5000" : process.env.REACT_APP_PROD_API;
-};
+export const getDomain = () =>
+  isInDevelopment() ? "http://localhost:5000" : process.env.REACT_APP_PROD_API;
+
 export const login = async (loginData: LoginData) =>
   await axios.post(`${getDomain()}/api/auth/login`, loginData);
 
