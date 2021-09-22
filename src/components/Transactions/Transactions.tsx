b965dc6f6b67b7beed46e266d78eabbac772b0d5
Akhere Ihoeghinlan <101akhere5@gmail.com>
2021-09-22 15:48:16 +0100

    refactor transactions component

--- src/components/Transactions/Transactions.tsx
+++ src/components/Transactions/Transactions.tsx
@@ -9,37 +9,6 @@ import Skeleton from "./Skeleton/Skeleton";
 
 const Transactions: FC<{ transactions: ITransaction[]; isLoading: boolean }> =
   ({ transactions, isLoading }) => {
-    if (isLoading) {
-      return (
-        <TransactionList>
-          <tbody>
-            <tr>
-              <td>
-                <h2 style={{ padding: "0.5rem 1rem 0rem" }}>Transactions</h2>
-              </td>
-            </tr>
-            {range(5).map((i) => (
-              <Skeleton key={i} />
-            ))}
-          </tbody>
-        </TransactionList>
-      );
-    }
-
-    if (!transactions || !transactions.length) {
-      return (
-        <TransactionList>
-          <tbody>
-            <tr>
-              <td style={{ padding: "1rem" }}>
-                <h2>No Transactions</h2>
-              </td>
-            </tr>
-          </tbody>
-        </TransactionList>
-      );
-    }
-
     return (
       <TransactionList>
         <tbody>
@@ -48,13 +17,29 @@ const Transactions: FC<{ transactions: ITransaction[]; isLoading: boolean }> =
               <h2 style={{ padding: "0.5rem 1rem 0rem" }}>Transactions</h2>
             </td>
           </tr>
-          {transactions.map((transaction, index) => (
-            <Transaction
-              key={transaction._id}
-              {...transaction}
-              hideSeparator={index === transactions.length - 1}
-            />
-          ))}
+          {!isLoading && range(5).map((i) => <Skeleton key={i} />)}
+          {transactions.length === 0 && isLoading && (
+            <tr>
+              <td>
+                <div
+                  style={{
+                    padding: "1rem 2rem 2rem 2rem",
+                    textAlign: "center",
+                  }}
+                >
+                  <h3>No Transactions</h3>
+                </div>
+              </td>
+            </tr>
+          )}
+          {!isLoading &&
+            transactions.map((transaction, index) => (
+              <Transaction
+                key={transaction._id}
+                {...transaction}
+                hideSeparator={index === transactions.length - 1}
+              />
+            ))}
         </tbody>
       </TransactionList>
     );
